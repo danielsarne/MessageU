6a8fc8d9fd520a6abe6cbafa98039cd2b9c0f54e
unknown <daniel@sarne.co.il>
2021-10-31 16:28:53 +0200

    moved client logics to class

---
+++ client/MessageU/Client.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "User.h"
+#include "ClientCommunicator.h"
+#include "RequestBuilder.h"
+#include "ClientInteractor.h"
+#include "Message.h"
+#include <vector>
+
+class Client
+{
+private:
+	vector<User> userList;
+	vector<Message> messageList;
+	ClientCommunicator comm;
+	vector<RequestBuilder*> requestBuilders;
+	ClientInteractor* interactor;
+public:
+	Client();
+	~Client();
+	/// <summary>
+	/// The function runs the logics of the client.
+	/// </summary>
+	void run();
+
+};
+
